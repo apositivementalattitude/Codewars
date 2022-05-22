4f4234fc40ef0a886d155c396ba240a1b65c40dd
Eric Lee <eazylee92@gmail.com>
2022-05-22 07:23:02 -0700

    added 05222022_roundUpToFive

---
+++ 05222022_roundUpToFive/js/main.js
@@ -0,0 +1,8 @@
+//https://github.com/apositivementalattitude/Codewars.git
+
+//Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+function roundToNext5(n){
+    // ...
+    return Math.ceil(n/5) * 5;
+  }
