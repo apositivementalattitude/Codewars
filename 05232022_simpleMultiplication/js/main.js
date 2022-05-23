d895129fe5faf8be466139fec16ded7a9a642908
Eric Lee <eazylee92@gmail.com>
2022-05-23 15:59:47 -0700

    added 05232022_simpleMultiplication

---
+++ 05232022_simpleMultiplication/js/main.js
@@ -0,0 +1,11 @@
+//https://github.com/apositivementalattitude/Codewars.git
+
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8
+  }else {
+    return number * 9
+  }
+}
