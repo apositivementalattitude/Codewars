b4fa2f19e872b29d19030f016220d46e0dcd60b3
Eric Lee <eazylee92@gmail.com>
2022-05-30 08:09:11 -0700

    added 05302022_replaceWithExclamation

---
+++ 05302022_replaceWithExclamation/js/main.js
@@ -0,0 +1,9 @@
+//https://github.com/apositivementalattitude/Codewars.git
+
+//Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+function replace(s){
+    //coding and coding....
+    return s.replace(/[aeiouAEIOU]/gi, '!')
+    
+  }
