e2ea413a16655d5ab1c9fc284920af7b51f2d1db
Eric Lee <eazylee92@gmail.com>
2022-06-11 18:50:28 -0700

    added 06112022_squaringAnArg

---
+++ 06112022_squaringAnArg/js/main.js
@@ -0,0 +1,9 @@
+//https://github.com/apositivementalattitude/Codewars.git
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+
+function square (n) {
+    return n**2
+  }
