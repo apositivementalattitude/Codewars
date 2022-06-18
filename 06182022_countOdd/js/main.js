e0b37897eb0796e12d16836cc8388ca7e65e12f9
Eric Lee <eazylee92@gmail.com>
2022-06-18 10:37:12 -0700

    added 06182022_countOdd

---
+++ 06182022_countOdd/js/main.js
@@ -0,0 +1,9 @@
+//https://github.com/apositivementalattitude/Codewars.git
+
+//Given a number n, return the number of positive odd numbers below n, EASY!
+
+
+function oddCount(n){
+    // your code here
+    return Math.floor(n/2)
+  }
